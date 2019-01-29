0d9835e737be9be0a17437f23a04e410251c889d
yinyifu <yifuyin@buffalo.edu>
2019-01-29 12:20:04 -0500

    Fixed error with refresh white label error

---
+++ src/main/java/org/onedatashare/server/controller/DefaultRouter.java
@@ -0,0 +1,10 @@
+package org.onedatashare.server.controller;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class DefaultRouter {
+
+
+}
--- src/main/java/org/onedatashare/server/controller/RestartJobController.java
+++ src/main/java/org/onedatashare/server/controller/RestartJobController.java
@@ -1,10 +1,12 @@
 package org.onedatashare.server.controller;
 
+import org.onedatashare.server.model.core.Job;
 import org.onedatashare.server.model.useraction.UserAction;
 import org.onedatashare.server.service.ResourceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/stork/restart")
@@ -14,7 +16,7 @@ public class RestartJobController {
     private ResourceServiceImpl resourceService;
 
     @PostMapping
-    public Object restartJob(@RequestHeader HttpHeaders headers, @RequestBody UserAction userAction){
+    public Mono<Job> restartJob(@RequestHeader HttpHeaders headers, @RequestBody UserAction userAction){
         String cookie = headers.getFirst("cookie");
         //System.out.println("Job to restart :" + userAction.job_id);
         return resourceService.restartJob(cookie, userAction);
