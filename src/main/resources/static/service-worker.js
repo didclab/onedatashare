357e68e20814ab2d0d6359843c4f2ab06b18f84a
kiranprabhakar135 <kiranprabhakar135@gmail.com>
2019-04-09 12:48:01 -0400

    Merge branch 'master' of https://github.com/didclab/onedatashare-spring into ShareLink
    # Conflicts:
    #	src/main/resources/application.properties
    #	src/main/resources/static/asset-manifest.json
    #	src/main/resources/static/index.html
    #	src/main/resources/static/service-worker.js

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f532e1271bf62b7eaffeec9d9b11cdc1.js"
+  "/precache-manifest.f67984a300edfba931a0418f66c8ca5c.js"
 );
 
 workbox.clientsClaim();
