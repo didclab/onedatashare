23bd31b666e1b6f3832c45efd9bf2945831a13c5
kiranprabhakar135 <kiranprabhakar135@gmail.com>
2019-04-09 12:41:40 -0400

    Merge branch 'master' of https://github.com/didclab/onedatashare-spring into ShareLink
    # Conflicts:
    #	src/main/resources/static/asset-manifest.json
    #	src/main/resources/static/index.html
    #	src/main/resources/static/service-worker.js

--- src/main/react-front-end/src/views/Transfer/EndpointBrowseComponent.js
+++ src/main/react-front-end/src/views/Transfer/EndpointBrowseComponent.js
@@ -344,11 +344,6 @@ export default class EndpointBrowseComponent extends Component {
 							<svg width="24" height="24" viewBox="0 0 24 24">
 								<path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
 							</svg>								
-							
-							{/* <FileCopyOutlined fontSize={"small"} onClick = {() => {
-								
-							}}>
-							</FileCopyOutlined> */}
 							</CopyToClipboard>
 	          </DialogContent>
 	          <DialogActions>
