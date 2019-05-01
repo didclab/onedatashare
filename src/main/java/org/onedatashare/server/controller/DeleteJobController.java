4290dddd97962fce2d28b4a2eec446aafad0b1b2
Atul Singh <atulsinghjnv@gmail.com>
2019-05-01 18:36:21 -0400

    DeleteJobController

--- src/main/java/org/onedatashare/server/controller/DeleteJobController.java
+++ src/main/java/org/onedatashare/server/controller/DeleteJobController.java
@@ -9,16 +9,28 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/stork/deleteJob")
 public class DeleteJobController {
 
     @Autowired
     private ResourceServiceImpl resourceService;
 
+    private static final String EMAIL_PARAM = "email";
+    private static final String HASH_PARAM = "hash";
+
     @PostMapping
     public Mono<Job> restartJob(@RequestHeader HttpHeaders headers, @RequestBody UserAction userAction){
-        String cookie = headers.getFirst("cookie");
-//        System.out.println("Job to delete :" + userAction.job_id);
+        String temp = headers.getFirst("cookie");
+        if(temp == null){
+            //System.out.println("Email: "+userAction.getEmail()+" Hash: "+userAction.getPassword());
+            if(userAction.getEmail()!=null && userAction.getPassword()!=null &&
+                    !userAction.getEmail().equalsIgnoreCase("") && !userAction.getPassword().equalsIgnoreCase("")){
+                temp = EMAIL_PARAM + "=" + userAction.getEmail() + "; " +
+                        HASH_PARAM + "=" + userAction.getPassword();
+            }
+        }
+        String cookie = temp;
         return resourceService.deleteJob(cookie, userAction);
     }
 }
