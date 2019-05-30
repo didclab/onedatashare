baeb916189fb1bc51f16489b72c11d5e3c077d26
Linus Castelino <castelinolinus@gmail.com>
2019-05-29 20:36:09 -0400

    Added doc comments

--- src/main/java/org/onedatashare/server/model/core/JobDetails.java
+++ src/main/java/org/onedatashare/server/model/core/JobDetails.java
@@ -4,6 +4,13 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * Model to hold response of queue and history pages.
+ *
+ * Holds list of jobs for the current page number (pagination) on queue page the user is viewing
+ * and the total count of jobs related to the user to generate page numbers.
+ * (totalCount holds the count of all jobs in the database for history page)
+ */
 @Data
 public class JobDetails{
 
