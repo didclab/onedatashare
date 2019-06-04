97aab03b4ee5c40133e2d5a07aa9a42b76d0d35c
Javier <jafalca>
2019-06-04 12:48:54 -0400

    Updated build files

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7668ce9464e08da1be3c8addd5652e9c.js"
+  "/precache-manifest.590dfc0bf36a0c70862f58c940d9c00a.js"
 );
 
 workbox.clientsClaim();
