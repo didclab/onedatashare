520b91e8699cbbe06e0bf1f439380c700af76dec
Linus Castelino <castelinolinus@gmail.com>
2019-06-14 00:16:35 -0400

    Created constants class

--- src/main/java/org/onedatashare/server/model/core/ODSConstants.java
+++ src/main/java/org/onedatashare/server/model/core/ODSConstants.java
@@ -1,4 +1,14 @@
 package org.onedatashare.server.model.core;
 
 public class ODSConstants {
+
+    public static final String DROPBOX_URI_SCHEME = "dropbox://";
+    public static final String DRIVE_URI_SCHEME = "googledrive:/";
+    public static final String SFTP_URI_SCHEME = "sftp://";
+    public static final String FTP_URI_SCHEME = "sftp://";
+    public static final String SCP_URI_SCHEME = "scp://";
+
+    public static final String UPLOAD_IDENTIFIER = "Upload";
+
+
 }
