b8c2e6d01bc2118b8aa6ccaf66eaba93ccac3acf
Aashish Jain <aashish_jain@live.com>
2019-06-27 12:22:25 -0400

    Removed TODO

--- src/main/java/org/onedatashare/server/service/HttpFileService.java
+++ src/main/java/org/onedatashare/server/service/HttpFileService.java
@@ -65,7 +65,6 @@ public class HttpFileService implements ResourceService<HttpResource> {
     }
 
     @Override
-    /* TODO: handle at the front-end */
     public Mono<String> download(String cookie, UserAction userAction) {
         return null;
     }
