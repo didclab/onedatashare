1bb69c7bbbfb7982390e4a0788db02f8fbe21782
kiranprabhakar135 <kiranprabhakar135@gmail.com>
2019-10-06 00:11:27 -0400

    comitting the build

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b91a1d6141d559a0400bb0796b38443.js"
+  "/precache-manifest.9c023e4d06941fb30a31481c6e264f18.js"
 );
 
 workbox.clientsClaim();
