f68470e021a614f6df54cbc0ad93113a4af8b24d
Aashish Jain <aashish_jain@live.com>
2019-10-15 12:42:37 -0400

    added frontend fixes for oauth login failure, built UI

---
+++ src/main/resources/static/precache-manifest.c82d378a1990d0a674ca11028cc821ac.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "99e0e744504ad836abb3b03b11746fd4",
+    "url": "/static/media/privacy.99e0e744.txt"
+  },
+  {
+    "revision": "2921da23060c5b948c67",
+    "url": "/static/css/main.cb7f2429.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e87424bb9d4d2108be9f",
+    "url": "/static/js/2.5e404f14.chunk.js"
+  },
+  {
+    "revision": "94fb798542b7654eed3be71944f22495",
+    "url": "/static/media/fast.94fb7985.png"
+  },
+  {
+    "revision": "c3ec87a720931fdc780734c78fa2680d",
+    "url": "/static/media/easy.c3ec87a7.png"
+  },
+  {
+    "revision": "84514f60b57f0fa8f4dbf0445ac20b44",
+    "url": "/static/media/endtoend.84514f60.png"
+  },
+  {
+    "revision": "fcfd05a0049d62620ef86d72924cdc28",
+    "url": "/static/media/precise.fcfd05a0.png"
+  },
+  {
+    "revision": "2558a94238b69fb43e7fcef6a8f0501e",
+    "url": "/static/media/interoperation.2558a942.png"
+  },
+  {
+    "revision": "0f6f085b9b31bff99fd098559a759a5d",
+    "url": "/static/media/NSF_Logo.0f6f085b.png"
+  },
+  {
+    "revision": "ac8a9d3e518e93145081c1c707cf9618",
+    "url": "/static/media/ub.ac8a9d3e.png"
+  },
+  {
+    "revision": "cdf166b219a70c18c78104401e14850a",
+    "url": "/static/media/terms.cdf166b2.txt"
+  },
+  {
+    "revision": "2921da23060c5b948c67",
+    "url": "/static/js/main.8cede731.chunk.js"
+  },
+  {
+    "revision": "240cb46ba528e6b393e4e40dcd2f91fe",
+    "url": "/static/media/s3.240cb46b.png"
+  },
+  {
+    "revision": "d37df03f5a21b720f133e1d7bcca29b7",
+    "url": "/static/media/s2.d37df03f.png"
+  },
+  {
+    "revision": "b79f2dc0cf2628cee3e0afcaa22d0ce9",
+    "url": "/static/media/s4.b79f2dc0.png"
+  },
+  {
+    "revision": "0066c9f5fb6311639ceff2d8b1fc8813",
+    "url": "/static/media/s5.0066c9f5.png"
+  },
+  {
+    "revision": "86c1f97abb212a6b0c9b751b6a42a067",
+    "url": "/static/media/s6.86c1f97a.png"
+  },
+  {
+    "revision": "be1fb98e0bf2939c9e44c2d150b952f3",
+    "url": "/static/media/s1.be1fb98e.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "e87424bb9d4d2108be9f",
+    "url": "/static/css/2.bb75b0ff.chunk.css"
+  },
+  {
+    "revision": "37ed7c79b7750cb455d33f5a95543ce7",
+    "url": "/index.html"
+  }
+];
