0437f8fc9ff56d47aa12e8e838e998b9b9111bd0
Linus Castelino <castelinolinus@gmail.com>
2019-11-03 05:55:08 -0500

    Added Get-Started Page

---
+++ src/main/resources/static/precache-manifest.73b956b5d79b0085acadaafa85796d36.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "db070790196f7a6d9b2ba6a779c9ec30",
+    "url": "/static/media/gs3.db070790.png"
+  },
+  {
+    "revision": "4a3a623dfe680e45ba6c",
+    "url": "/static/css/main.20d0152a.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "28d9a41c9584eb78ee54",
+    "url": "/static/js/2.74159173.chunk.js"
+  },
+  {
+    "revision": "94fb798542b7654eed3be71944f22495",
+    "url": "/static/media/fast.94fb7985.png"
+  },
+  {
+    "revision": "c3ec87a720931fdc780734c78fa2680d",
+    "url": "/static/media/easy.c3ec87a7.png"
+  },
+  {
+    "revision": "84514f60b57f0fa8f4dbf0445ac20b44",
+    "url": "/static/media/endtoend.84514f60.png"
+  },
+  {
+    "revision": "fcfd05a0049d62620ef86d72924cdc28",
+    "url": "/static/media/precise.fcfd05a0.png"
+  },
+  {
+    "revision": "2558a94238b69fb43e7fcef6a8f0501e",
+    "url": "/static/media/interoperation.2558a942.png"
+  },
+  {
+    "revision": "0f6f085b9b31bff99fd098559a759a5d",
+    "url": "/static/media/NSF_Logo.0f6f085b.png"
+  },
+  {
+    "revision": "ac8a9d3e518e93145081c1c707cf9618",
+    "url": "/static/media/ub.ac8a9d3e.png"
+  },
+  {
+    "revision": "9e6de21ab62d56886922dbf09de97692",
+    "url": "/static/media/terms.9e6de21a.txt"
+  },
+  {
+    "revision": "960a95f312d8a345a2217381752c7ccc",
+    "url": "/static/media/privacy.960a95f3.txt"
+  },
+  {
+    "revision": "55c97f927d8bb5fb2438546a971ec300",
+    "url": "/static/media/background-texture.55c97f92.png"
+  },
+  {
+    "revision": "4f757923fda8da56ebfacf66166f8bd7",
+    "url": "/static/media/gs1.4f757923.png"
+  },
+  {
+    "revision": "38f19695b1cf750b9dffe568b38ac1f4",
+    "url": "/static/media/gs2.38f19695.png"
+  },
+  {
+    "revision": "4a3a623dfe680e45ba6c",
+    "url": "/static/js/main.d83d0acb.chunk.js"
+  },
+  {
+    "revision": "528bb28c581e37591a91d4c6e194dc9e",
+    "url": "/static/media/gs4.528bb28c.png"
+  },
+  {
+    "revision": "5fa0e8a857adf9b0526f51cc7fe25851",
+    "url": "/static/media/gs5.5fa0e8a8.png"
+  },
+  {
+    "revision": "6d64046eafa7978fc9425076aac53701",
+    "url": "/static/media/gs6.6d64046e.png"
+  },
+  {
+    "revision": "7672ab8bcf436a039e81c26c29e967bc",
+    "url": "/static/media/gs7.7672ab8b.png"
+  },
+  {
+    "revision": "d37df03f5a21b720f133e1d7bcca29b7",
+    "url": "/static/media/s2.d37df03f.png"
+  },
+  {
+    "revision": "be1fb98e0bf2939c9e44c2d150b952f3",
+    "url": "/static/media/s1.be1fb98e.png"
+  },
+  {
+    "revision": "240cb46ba528e6b393e4e40dcd2f91fe",
+    "url": "/static/media/s3.240cb46b.png"
+  },
+  {
+    "revision": "b79f2dc0cf2628cee3e0afcaa22d0ce9",
+    "url": "/static/media/s4.b79f2dc0.png"
+  },
+  {
+    "revision": "0066c9f5fb6311639ceff2d8b1fc8813",
+    "url": "/static/media/s5.0066c9f5.png"
+  },
+  {
+    "revision": "86c1f97abb212a6b0c9b751b6a42a067",
+    "url": "/static/media/s6.86c1f97a.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "28d9a41c9584eb78ee54",
+    "url": "/static/css/2.fd6282fe.chunk.css"
+  },
+  {
+    "revision": "c7bd04ef72dcecbd62c9247d65c17c42",
+    "url": "/index.html"
+  }
+];
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.711096771d089742785b3011c63242f4.js"
+  "/precache-manifest.73b956b5d79b0085acadaafa85796d36.js"
 );
 
 workbox.clientsClaim();
