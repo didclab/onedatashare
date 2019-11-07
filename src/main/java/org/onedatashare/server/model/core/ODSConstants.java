f61401311a4b016f3cf1411a8347b6e2efeb4946
Linus Castelino <castelinolinus@gmail.com>
2019-11-07 18:18:38 -0500

    Add URLs list and perform set operation in IndexFilter

--- src/main/java/org/onedatashare/server/model/core/ODSConstants.java
+++ src/main/java/org/onedatashare/server/model/core/ODSConstants.java
@@ -1,5 +1,11 @@
 package org.onedatashare.server.model.core;
 
+import edu.emory.mathcs.backport.java.util.Arrays;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ODSConstants {
 
     public static final String DROPBOX_URI_SCHEME = "dropbox:///";
@@ -21,5 +27,15 @@ public class ODSConstants {
     public static final String CERTIFICATEKEYALIAS = "tomcat";
 
     public static final int REDIRECTHOSTINGPORT = 80;
+
+    public static final String[] ODS_URIS_ARR = new String[]{"/", "/transfer", "/terms", "/policy", "/user", "/queue",
+                                                                "/clientsInfo", "/history", "/account", "/oauth",
+                                                                "/support", "/get-started"};
+    public static final Set<String> ODS_URIS_SET = new HashSet<String>();
+
+    static{
+        ODS_URIS_SET.addAll(Arrays.asList(ODS_URIS_ARR));
+    }
+
 
 }
