4e6bf0d2a752093b63997e82b077fd61a72d1efc
Linus Castelino <castelinolinus@gmail.com>
2019-11-10 02:35:34 -0500

    Fixed build error

--- src/main/react-front-end/src/views/Login/AccountControlComponent.js
+++ src/main/react-front-end/src/views/Login/AccountControlComponent.js
@@ -190,7 +190,7 @@ export default class AccountControlComponent extends Component {
 	render() {
 		const currentRoute = this.props.location.pathname
 		const { isSmall, loading, creatingAccount, signIn, forgotPasswordPressed, lostValidationCodePressed, rememberMeAccounts, redirectToSignIn } = this.state;
-		this.setState({signIn = Object.keys(rememberMeAccounts).length === 0 && currentRoute !== registerPageUrl});
+		this.setState({signIn : Object.keys(rememberMeAccounts).length === 0 && currentRoute !== registerPageUrl});
 		this.setState({creatingAccount: false});
 		this.setState({lostValidationCodePressed : false});
 		this.setState({forgotPasswordPressed : false});
