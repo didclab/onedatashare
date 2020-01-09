52d704efc28b3acc77d00aaa550122cfa68740a3
Anas <anas.ta.hamed@gmail.com>
2020-01-09 18:13:20 -0500

    fixed dependency issue in controller tests

---
+++ src/test/java/org/onedatashare/server/controller/QueueControllerTest.java
@@ -0,0 +1,15 @@
+package org.onedatashare.server.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MkdirController.class)
+class QueueControllerTest {
+
+    
+
+}
