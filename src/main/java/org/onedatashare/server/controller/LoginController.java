097fd8b3ed86664865d78770dd4defb9322ef04f
Aashish Jain <aashish_jain@live.com>
2020-03-06 15:41:22 -0500

    Added code for httponly cookie

--- src/main/java/org/onedatashare/server/controller/LoginController.java
+++ src/main/java/org/onedatashare/server/controller/LoginController.java
@@ -6,7 +6,11 @@ import org.onedatashare.server.model.util.Response;
 import org.onedatashare.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import static org.onedatashare.server.model.core.ODSConstants.*;
@@ -18,8 +22,18 @@ public class LoginController {
     private UserService userService;
 
     @RequestMapping(value = AUTH_ENDPOINT, method = RequestMethod.POST)
-    public Mono<LoginResponse> login(@RequestBody LoginControllerRequest request) {
-        return userService.login(request.getEmail(), request.getPassword());
+    public Mono<ResponseEntity> login(@RequestBody LoginControllerRequest request) {
+        return userService.login(request.getEmail(), request.getPassword())
+                .map(loginResponse -> {
+                    HttpHeaders responseHeaders = new HttpHeaders();
+                    StringBuilder cookieStringBuilder = new StringBuilder("ATOKEN=");
+                    cookieStringBuilder
+                            .append(loginResponse.getToken());
+                    responseHeaders.set(HttpHeaders.SET_COOKIE,
+                            cookieStringBuilder.toString());
+                    return ResponseEntity.ok().headers(responseHeaders).body(loginResponse);
+                });
+//        return userService.login(request.getEmail(), request.getPassword());
     }
 
     @RequestMapping(value = IS_REGISTERED_EMAIL_ENDPOINT, method = RequestMethod.POST)
