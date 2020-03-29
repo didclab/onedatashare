1fbe5c648e93190fe892cfe2f66b9227e89e9db4
Jacob <jgoldverg@gmail.com>
2020-03-29 00:36:20 -0400

    Converting to using Annotations as the ODS controller code needs to be annotated for OpenAPI3

--- src/main/java/org/onedatashare/server/ServerApplication.java
+++ src/main/java/org/onedatashare/server/ServerApplication.java
@@ -23,10 +23,29 @@
 
 package org.onedatashare.server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+        info = @Info(
+                title = "OneDataShare REST API",
+                version = "1.0",
+                description = "OpenAPI REST API documentation",
+                license = @License(name = "Apache-2.0", url = "https://github.com/didclab/onedatashare/blob/master/LICENSE"),
+                contact = @Contact(url = "http://localhost:8080", name = "OneDataShare Team", email = "admin@onedatashare.org")
+        ),
+        servers = {
+                @Server(
+                        description = "ODS backend",
+                        url = "http://localhost:8080"
+                )
+        }
+)
 @SpringBootApplication
 public class ServerApplication {
 
