81fc17c208e3910371822a27f9907c279c8c776d
Ta7ar <imonkallprofessional@gmail.com>
2020-08-26 22:06:37 -0400

    Refactored styling, fixed link-buttons deadspace issue, fixed back-to-signin button functionality on the login/ register pages.

--- src/main/react-front-end/src/views/Login/NewLoginComponent.js
+++ src/main/react-front-end/src/views/Login/NewLoginComponent.js
@@ -20,191 +20,244 @@
  ##**************************************************************
  */
 
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import { Link } from "react-router-dom";
 
-import React, {Component} from 'react';
-import Typography from '@material-ui/core/Typography';
-import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
-import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
 
-import PropTypes from 'prop-types';
+import { checkLogin } from "../../APICalls/APICalls.js";
 
-import { checkLogin} from '../../APICalls/APICalls.js';
+import { spaceBetweenStyle } from "../../constants.js";
+import { updateGAPageView } from "../../analytics/ga";
+import VisibilityOutlinedIcon from "@material-ui/icons/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@material-ui/icons/VisibilityOffOutlined";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
 
-
-import {spaceBetweenStyle} from '../../constants.js';
-import {updateGAPageView} from "../../analytics/ga";
-import VisibilityOutlinedIcon from '@material-ui/icons/VisibilityOutlined';
-import VisibilityOffOutlinedIcon from '@material-ui/icons/VisibilityOffOutlined';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import IconButton from '@material-ui/core/IconButton';
-
+const ODSLogoBlack = require("./images/logoBlack.png");
 
 export default class NewLoginComponent extends Component {
+  static propTypes = {
+    email: PropTypes.string,
+    createAccountPressed: PropTypes.func,
+    lostValidationCodePressed: PropTypes.func,
+    forgotPasswordPressed: PropTypes.func,
+    isLoading: PropTypes.func,
+    userLoggedIn: PropTypes.func,
+  };
 
-	static propTypes = {
-	  	email : PropTypes.string,
-	  	createAccountPressed: PropTypes.func,
-	  	lostValidationCodePressed: PropTypes.func,
-	  	forgotPasswordPressed: PropTypes.func,
-	  	isLoading: PropTypes.func,
-	  	userLoggedIn: PropTypes.func
-	}
-	
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      emailChecked: false,
+      error: false,
+      errorMessage: null,
+      remember: false,
+      isAuthenticated: false,
+      isPasswordVisible: false,
+    };
 
-	constructor(props){
-	    super(props);
-	    this.state = {
-	    	email: "",
-	    	password: "",
-	    	emailChecked: false,
-	    	error: false,
-	    	errorMessage: null,
-	    	remember: false,
-			isAuthenticated: false,
-			isPasswordVisible: false
-	    }
-	    
-	    this.emailValidated = false;
-	    this.onEmailNextClicked = this.onEmailNextClicked.bind(this);
-		this.onSignInClicked = this.onSignInClicked.bind(this);
-		this.handleShowPassword = this.handleShowPassword.bind(this);
-		this.handleHidePassword = this.handleHidePassword.bind(this);
-		updateGAPageView();
-	}
-	componentDidMount(){
-		if(this.props.email){
-			this.setState({email: this.props.email});
-		}
-	}
+    this.emailValidated = false;
+    this.onEmailNextClicked = this.onEmailNextClicked.bind(this);
+    this.onSignInClicked = this.onSignInClicked.bind(this);
+    this.handleShowPassword = this.handleShowPassword.bind(this);
+    this.handleHidePassword = this.handleHidePassword.bind(this);
+    updateGAPageView();
+  }
+  componentDidMount() {
+    if (this.props.email) {
+      this.setState({ email: this.props.email });
+    }
+  }
 
-	onEmailNextClicked(){
-		const { isLoading } = this.props;
-		isLoading(true);
+  onEmailNextClicked() {
+    const { isLoading } = this.props;
+    isLoading(true);
 
-	    checkLogin(this.state.email,
-	    	(success)=>{
-	    		isLoading(false);
-	    		this.setState({emailChecked: true});
-	    	},
-	    	(error)=>{
-	    		isLoading(false);
-	    		this.setState({emailChecked: false, error: true, errorMessage: "Email not found or server error."});
-	    	}
-	    );
-	}
+    checkLogin(
+      this.state.email,
+      (success) => {
+        isLoading(false);
+        this.setState({ emailChecked: true });
+      },
+      (error) => {
+        isLoading(false);
+        this.setState({
+          emailChecked: false,
+          error: true,
+          errorMessage: "Email not found or server error.",
+        });
+      }
+    );
+  }
 
-	onSignInClicked(){
-		const { isLoading, userLoggedIn } = this.props;
-		var { email, password, remember } = this.state;
-		isLoading(true);
+  onSignInClicked() {
+    const { isLoading, userLoggedIn } = this.props;
+    var { email, password, remember } = this.state;
+    isLoading(true);
 
-		userLoggedIn(email, password, remember, (error)=>{
-    		isLoading(false);
-    		console.log("error message", error);
-			  this.setState({error: true, errorMessage: "Wrong password or server error."});
-		});
-	}
+    userLoggedIn(email, password, remember, (error) => {
+      isLoading(false);
+      console.log("error message", error);
+      this.setState({
+        error: true,
+        errorMessage: "Wrong password or server error.",
+      });
+    });
+  }
+  /**
+   * @deprecated
+   */
 
-	handleShowPassword() {
-		this.setState({isPasswordVisible: true});
-	}
+  handleShowPassword() {
+    this.setState({ isPasswordVisible: true });
+  }
+  /**
+   * @deprecated
+   */
 
-	handleHidePassword() {	
-		this.setState({isPasswordVisible: false});
-	}
+  handleHidePassword() {
+    this.setState({ isPasswordVisible: false });
+  }
 
-	render(){
-		const { lostValidationCodePressed, forgotPasswordPressed } = this.props; 
-		const { emailChecked, email, password, error, errorMessage, isPasswordVisible } = this.state;
-		const handleChange = name => event => {
-		    this.setState({
-		      error: false,
-		      errorMessage: null,
-		      [name]: event.target.value,
-		    });
-		};
+  toggleShowPassword = () => {
+    this.setState({ isPasswordVisible: !this.state.isPasswordVisible });
+  };
 
-		return (<div >
-		{!emailChecked &&
-			<div className="enter-from-right slide-in">
+  render() {
+    const { lostValidationCodePressed, forgotPasswordPressed } = this.props;
+    const {
+      emailChecked,
+      email,
+      password,
+      error,
+      errorMessage,
+      isPasswordVisible,
+    } = this.state;
+    const handleChange = (name) => (event) => {
+      this.setState({
+        error: false,
+        errorMessage: null,
+        [name]: event.target.value,
+      });
+    };
 
-	      	<Typography style={{fontSize: "1.6em", marginBottom: "0.4em", textAlign: 'center'}}>
-	          Sign In
-	        </Typography>
-	        <Typography style={{fontSize: "1em", textAlign: 'center'}} color="textSecondary">
-	          Onedatashare
-	        </Typography>
+    return (
+      <div>
+        {!emailChecked && (
+          <div className="enter-from-right slide-in">
+            <Typography
+              style={{ fontSize: "1em", textAlign: "center" }}
+              color="textSecondary"
+            >
+              <img src={ODSLogoBlack} width="100px" alt="Onedatashare logo" />
+            </Typography>
+            <Typography
+              style={{
+                fontSize: "1.6em",
+                marginBottom: "0.4em",
+                textAlign: "center",
+                marginTop: "0.5em",
+              }}
+            >
+              Sign In
+            </Typography>
 
-	        <ValidatorForm
-                ref="email"
-                onSubmit={this.onEmailNextClicked}>
-            	<TextValidator
-            		error = {error}
-            		helperText = {errorMessage}
-								label="Email"
-								onChange={handleChange('email')}
-								id="email"
-								name="email"
-								value={email}
-								validators={['required', 'isEmail']}
-								errorMessages={['Please put email here', 'Can not understand email format']}
-		          	style={{width: "90%", margin: "5%"}}
+            <ValidatorForm ref="email" onSubmit={this.onEmailNextClicked}>
+              <TextValidator
+                error={error}
+                helperText={errorMessage}
+                label="Email"
+                onChange={handleChange("email")}
+                id="email"
+                name="email"
+                value={email}
+                validators={["required", "isEmail"]}
+                errorMessages={[
+                  "Email field cannot be empty",
+                  "Cannot understand email format",
+                ]}
+                style={{ width: "90%", margin: "5%" }}
+                InputProps={{
+                  style: { fontSize: 14 },
+                }}
               />
-		        <CardActions style={spaceBetweenStyle}>
-			        <Button size="large" variant="outlined" color="primary">
-								<Link to="/account/register">
-                   Create Account
+              <CardActions style={spaceBetweenStyle}>
+                <Link to="/account/register">
+                  <Button size="medium" variant="outlined" color="primary">
+                    Create Account
+                  </Button>
                 </Link>
-							</Button>
-			        <Button size="large" variant="contained" color="primary"  type="submit" >
-			          Next
-			        </Button>
-			    </CardActions>
 
-            	</ValidatorForm>
-		    </div>
-	    }
-	    {emailChecked &&
-	    	<div className="enter-from-right slide-in">
-	    	<Typography style={{fontSize: "1.6em", marginBottom: "0.4em"}}>
-	          Hi {email.substring(0, email.indexOf('@'))}!
-	        </Typography>
-	        <Button size="large" style={{borderRadius: '20px'}} variant="outlined" color="primary" onClick={()=>this.setState({emailChecked: false})}>
-	          {email}
-	        </Button>
+                <Button
+                  size="medium"
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                >
+                  Next
+                </Button>
+              </CardActions>
+            </ValidatorForm>
+          </div>
+        )}
+        {emailChecked && (
+          <div className="enter-from-right slide-in">
+            <Typography style={{ fontSize: "1.6em", marginBottom: "0.4em" }}>
+              Hi {email.substring(0, email.indexOf("@"))}!
+            </Typography>
+            <Button
+              size="medium"
+              style={{ borderRadius: "20px" }}
+              variant="outlined"
+              color="primary"
+              onClick={() => this.setState({ emailChecked: false })}
+            >
+              {email}
+            </Button>
 
-	        <ValidatorForm
-                ref="password"
-                onSubmit={this.onSignInClicked}>
-            <TextValidator
-            		error = {error}
-            		helperText = {errorMessage}
-            		id="Password"
-                    label="Enter Your Password"
-                    onChange={handleChange('password')}
-                    type={isPasswordVisible ? "text" : "password"}
-                    name="password"
-	          		value={password}
-                    validators={['required']}
-                    errorMessages={['Where is password?']}
-					style={{width: "100%", marginTop: "5%", marginBottom: "5%"}}
-					InputProps={{
-						endAdornment: <React.Fragment>
-							<InputAdornment position="end">
-								<IconButton
-									aria-label="toggle password visibility"
-									onMouseDown={this.handleShowPassword}
-									onMouseUp={this.handleHidePassword}
-								>
-								{isPasswordVisible ? <VisibilityOutlinedIcon /> : <VisibilityOffOutlinedIcon />}
-								</IconButton>
-							</InputAdornment>
-							</React.Fragment>
-					}}  
-            />
-            {/* <FormControlLabel
+            <ValidatorForm ref="password" onSubmit={this.onSignInClicked}>
+              <TextValidator
+                error={error}
+                helperText={errorMessage}
+                id="Password"
+                label="Enter Your Password"
+                onChange={handleChange("password")}
+                type={isPasswordVisible ? "text" : "password"}
+                name="password"
+                value={password}
+                validators={["required"]}
+                errorMessages={["Password Field Cannot Be Empty"]}
+                style={{ width: "100%", marginTop: "5%", marginBottom: "5%" }}
+                InputProps={{
+                  style: {
+                    fontSize: 14,
+                  },
+                  endAdornment: (
+                    <React.Fragment>
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={this.toggleShowPassword}
+                        >
+                          {isPasswordVisible ? (
+                            <VisibilityOutlinedIcon />
+                          ) : (
+                            <VisibilityOffOutlinedIcon />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    </React.Fragment>
+                  ),
+                }}
+              />
+              {/* <FormControlLabel
 			control={
 	            <Checkbox checked={remember} value="remember"
 	            onChange={(event)=>{
@@ -214,31 +267,30 @@ export default class NewLoginComponent extends Component {
 	            />
 	        } label="Remember"/> */}
 
-	        <CardActions style={{...spaceBetweenStyle, marginBottom: '20px'}}>
-						<Button 
-							size="small" 
-							variant="outlined"
-							color="primary"
-		        	onClick={()=>forgotPasswordPressed(email)}>
-		          Forgot Password?
-		        </Button>
-						<Button 
-							size="small" 
-							variant="outlined"
-							color="primary"
-		        	onClick={()=>lostValidationCodePressed(email)}>
-		          Lost Validation Email?
-		        </Button>
-		    </CardActions>
-
-		    <Button size="large" variant="contained" color="primary" type="submit" style={{width: '100%'}}>
-	        	Next
-	      </Button>
-
-		    </ValidatorForm>
-	        
-		    </div>
-		}
-		</div>);
-	}
-} 
+              <CardActions
+                style={{ ...spaceBetweenStyle, marginBottom: "20px" }}
+              >
+                <Button
+                  size="medium"
+                  variant="outlined"
+                  color="primary"
+                  onClick={() => forgotPasswordPressed(email)}
+                >
+                  Forgot Password?
+                </Button>
+                <Button
+                  size="medium"
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                >
+                  Next
+                </Button>
+              </CardActions>
+            </ValidatorForm>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
