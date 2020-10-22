2b41d8ad26a6bef62089d0d5970a8e891e744cd8
Mythreyi <58193207+mythreyi1996@users.noreply.github.com>
2020-10-22 11:02:28 -0400

    Implement Google Drive functionality and remove Gdrive Session (#342)
    * Implement Google Drive functionality
    
    * Remove Gdrive session.
    
    Co-authored-by: jacob goldverg <jgoldverg@gmail.com>

--- src/main/java/org/onedatashare/server/module/googledrive/GDriveResource.java
+++ src/main/java/org/onedatashare/server/module/googledrive/GDriveResource.java
@@ -55,14 +55,7 @@ public class GDriveResource extends Resource<GDriveSession, GDriveResource> {
     public GDriveResource(Drive drive, String path, String id) {
         super(path, id);
         this.drive = drive;
-    }
-    protected GDriveResource(GDriveSession session, String path, String id) {
-        super(session, path, id);
-    }
-    protected GDriveResource(GDriveSession session, String path) {
-        super(session, path,null);
-    }
-
+      
     public Mono<GDriveResource> mkdir() {
         return Mono.create(s -> {
             try {
