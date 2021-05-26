c807b8eded82847404bcb6834a24cd4e09d66090
Jacob Goldverg <jgoldverg@gmail.com>
2021-05-26 19:22:57 -0400

    added service name and logger for connectionString

--- src/main/java/org/onedatashare/server/config/MongoConfig.java
+++ src/main/java/org/onedatashare/server/config/MongoConfig.java
@@ -1,14 +1,17 @@
 package org.onedatashare.server.config;
 
+import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoConfig {
+    private final static Logger logger = Logger.getLogger(MongoConfig.class);
 
     @Value("${spring.data.mongodb.username}")
     String username;
@@ -24,11 +27,12 @@ public class MongoConfig {
 
     @Bean
     public MongoClient mongoClient(){
-        String template = "mongodb://%s:%s@%s/%s?ssl=true&replicaSet=rs0&readpreference=%s";
+        String template = "mongodb://%s:%s@%s/onedatashare?ssl=true&replicaSet=rs0&readpreference=%s";
         String readPreference = "secondaryPreferred";
-        String connectionString = String.format(template, username, password, clusterEndpoint, database,readPreference);
+        String connectionString = String.format(template, username, password, clusterEndpoint,readPreference);
         MongoClientURI clientURI = new MongoClientURI(connectionString);
         MongoClient mongoClient = new MongoClient(clientURI.getURI());
+        logger.info(connectionString);
         return mongoClient;
     }
 }
