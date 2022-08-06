1d23ce06e77c79538cdee17c73012b303ab3980c
Jacob Goldverg <jgoldverg@gmail.com>
2022-08-06 02:06:10 -0400

    forgot to convert json to proper names

--- src/main/java/org/onedatashare/server/model/requestdata/MonitorData.java
+++ src/main/java/org/onedatashare/server/model/requestdata/MonitorData.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 @Data
 public class MonitorData {
-    List<InfluxData> measurements;
-    BatchJobData batchJobData;
+    List<InfluxData> measurementData;
+    BatchJobData jobData;
 }
