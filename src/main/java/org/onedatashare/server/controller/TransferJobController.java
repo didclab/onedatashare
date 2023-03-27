4e063a161de69011412d2f216ceaa110bcc94aa1
Amisha Sinha <amisha.sinha@bridgesgi.com>
2023-03-27 12:13:58 -0400

    Pom file version update and implementation of job termination controller

--- src/main/java/org/onedatashare/server/controller/TransferJobController.java
+++ src/main/java/org/onedatashare/server/controller/TransferJobController.java
@@ -1,15 +1,15 @@
 package org.onedatashare.server.controller;
 
+import org.onedatashare.server.model.request.StopRequest;
 import org.onedatashare.server.model.request.TransferJobRequest;
 import org.onedatashare.server.model.response.TransferJobSubmittedResponse;
 import org.onedatashare.server.service.TransferJobService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 import java.security.Principal;
@@ -24,6 +24,9 @@ import java.security.Principal;
 public class TransferJobController {
     @Autowired
     private TransferJobService transferJobService;
+    public TransferJobController(TransferJobService transferJobService) {
+        this.transferJobService = transferJobService;
+    }
 
     Logger logger = LoggerFactory.getLogger(TransferJobController.class);
 
@@ -33,4 +36,11 @@ public class TransferJobController {
         logger.info("Recieved request: " + request.toString());
         return principalMono.flatMap(p -> transferJobService.submitTransferJobRequest(p.getName(), request));
     }
+    @PostMapping
+    public  Mono<Void> stopJob(@RequestBody StopRequest stopRequest){
+        return transferJobService.stopTransferJob(stopRequest)
+                .onErrorResume(e -> Mono.error(new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                        "Failed to stop job execution")));
+    }
+
 }
---
+++ src/main/java/org/onedatashare/server/model/request/StopRequest.java
@@ -0,0 +1,10 @@
+package org.onedatashare.server.model.request;
+
+import lombok.Data;
+
+@Data
+public class StopRequest {
+    Long jobId;
+    String transferNodeName;
+
+}
