c25d0ee18c692f72054f94117cc0b5eeda789707
Jacob Goldverg <jgoldverg@gmail.com>
2023-05-18 23:35:17 -0400

    including tests for the http resource, run a local http server such as NGINX or have a remote one running to test with

--- src/test/java/org/onedatashare/server/module/HttpResourceTest.java
+++ src/test/java/org/onedatashare/server/module/HttpResourceTest.java
@@ -1,2 +1,108 @@
-package org.onedatashare.server.module;public class HttpResourceTest {
+package org.onedatashare.server.module;
+
+import org.apache.commons.vfs2.FileSystemException;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.onedatashare.server.model.core.Stat;
+import org.onedatashare.server.model.credential.AccountEndpointCredential;
+import org.onedatashare.server.model.filesystem.operations.ListOperation;
+import reactor.core.publisher.Mono;
+
+public class HttpResourceTest {
+
+    HttpResource testObj;
+
+    public AccountEndpointCredential testAccountCredential(){
+        AccountEndpointCredential accountEndpointCredential = new AccountEndpointCredential();
+        accountEndpointCredential.setAccountId("test_account_id");
+        accountEndpointCredential.setUri("http://localhost:80/");
+        accountEndpointCredential.setUsername("test");
+        accountEndpointCredential.setSecret("test");
+        return accountEndpointCredential;
+    }
+
+    @Test
+    public void testListingRootOfNginx() {
+        //String credId, String path, String id
+        this.testObj = new HttpResource(this.testAccountCredential());
+        ListOperation listOperation = new ListOperation("", "", "");
+        Mono<Stat> monoStat = this.testObj.list(listOperation);
+        Stat result = monoStat.block();
+        Assert.assertNotNull(result);
+        Assert.assertNotNull(result.getFilesList());
+        Assert.assertNotNull(result.getName());
+        Assert.assertNotNull(result.getId());
+        Assert.assertNotNull(result.getSize());
+    }
+
+    @Test
+    public void testListingRootThreeFilesTwoFolders(){
+        this.testObj = new HttpResource(this.testAccountCredential());
+        ListOperation listOperation = new ListOperation("", "", "");
+        Mono<Stat> monoStat = this.testObj.list(listOperation);
+        Stat result = monoStat.block();
+        Assert.assertNotNull(result);
+        Assert.assertNotNull(result.getFiles());
+        for(Stat child : result.getFiles()){
+            System.out.println("Name: " + child.getName() + " Size: " + child.getSize());
+        }
+        Assert.assertTrue(result.getFiles().length == 5);
+    }
+
+    @Test
+    public void testListConcurrencyDataSet(){
+        this.testObj = new HttpResource(this.testAccountCredential());
+        ListOperation listOperation = new ListOperation("", "concurrency/", "");
+        Mono<Stat> monoStat = this.testObj.list(listOperation);
+        Stat result = monoStat.block();
+        Assert.assertNotNull(result);
+        Assert.assertNotNull(result.getFiles());
+        for(Stat child : result.getFiles()){
+            System.out.println("Name: " + child.getName() + " Size: " + child.getSize());
+        }
+        Assert.assertTrue(result.getFiles().length == 70);
+    }
+
+    @Test
+    public void testListParallelDataSet(){
+        this.testObj = new HttpResource(this.testAccountCredential());
+        ListOperation listOperation = new ListOperation("", "parallel/", "");
+        Mono<Stat> monoStat = this.testObj.list(listOperation);
+        Stat result = monoStat.block();
+        Assert.assertNotNull(result);
+        Assert.assertNotNull(result.getFiles());
+        for(Stat child : result.getFiles()){
+            System.out.println("Name: " + child.getName() + " Size: " + child.getSize());
+        }
+        Assert.assertTrue(result.getFiles().length == 7);
+    }
+
+    @Test
+    public void testListParallelDataSetUsingId(){
+        this.testObj = new HttpResource(this.testAccountCredential());
+        ListOperation listOperation = new ListOperation("", null, "parallel/");
+        Mono<Stat> monoStat = this.testObj.list(listOperation);
+        Stat result = monoStat.block();
+        Assert.assertNotNull(result);
+        Assert.assertNotNull(result.getFiles());
+        for(Stat child : result.getFiles()){
+            System.out.println("Name: " + child.getName() + " Size: " + child.getSize());
+        }
+        Assert.assertTrue(result.getFiles().length == 7);
+    }
+
+    @Test
+    public void testListConcurrencyDataSetUsingId(){
+        this.testObj = new HttpResource(this.testAccountCredential());
+        ListOperation listOperation = new ListOperation("", null, "parallel/");
+        Mono<Stat> monoStat = this.testObj.list(listOperation);
+        Stat result = monoStat.block();
+        Assert.assertNotNull(result);
+        Assert.assertNotNull(result.getFiles());
+        for(Stat child : result.getFiles()){
+            System.out.println("Name: " + child.getName() + " Size: " + child.getSize());
+        }
+        Assert.assertTrue(result.getFiles().length == 7);
+    }
+
 }
