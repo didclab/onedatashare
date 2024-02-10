434cb22f3f997cde5ca6127d51d4296e5bca288c
Ritika Rekhi <ritikarekhi@Ritikas-MacBook-Air.local>
2024-02-09 19:28:35 -0500

    Removed request handler

--- src/main/java/org/onedatashare/server/config/ApplicationSecurityConfig.java
+++ src/main/java/org/onedatashare/server/config/ApplicationSecurityConfig.java
@@ -53,7 +53,6 @@ public class ApplicationSecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        ServerCsrfTokenRequestAttributeHandler requestHandler = new ServerCsrfTokenRequestAttributeHandler();
         return http
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .authenticationManager(odsAuthenticationManager)
@@ -72,7 +71,7 @@ public class ApplicationSecurityConfig {
                 .exceptionHandling(exceptionHandlingSpec ->
                         exceptionHandlingSpec.authenticationEntryPoint(this::authenticationFailedHandler)
                                 .accessDeniedHandler(this::accessDeniedHandler))
-                .csrf(csrfSpec -> csrfSpec.csrfTokenRequestHandler(requestHandler).disable())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .build();
 
     }
