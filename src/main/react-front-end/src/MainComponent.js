6d0f97e57eb192d8666c04f5eca6e22fdcae87bb
Jayakrishna Goud Gandhamalla <gandhamallajayakrishna@hotmail.com>
2024-03-02 17:54:26 -0500

    Removed Slick Carousel

--- src/main/react-front-end/src/MainComponent.js
+++ src/main/react-front-end/src/MainComponent.js
@@ -72,9 +72,6 @@ export default class MainComponent extends Component {
     return (
       <div className="App">
 
-        <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-
         <NavbarComponent key={isLoggedIn} login={isLoggedIn} email={store.getState().email}></NavbarComponent>
 
 
